d6bc8b2b3aa3f29065d23e91905643917b4de73c
Sam Lerner <lerner98@gmail.com>
2019-08-29 17:57:45 -0400

    Added user, interests, and friends endpoints

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, url_for
+from flask import Flask, jsonify
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
 	return app.send_static_file('index.html')
+
+@app.route('/user/<twitter_handle')
+def user(twitter_handle):
+	return jsonify({
+		'user_id': '1234',
+		'name': 'Sam Lerner',
+		'bio': 'OSU Computer Science. A in 5914',
+		'avatar': 'test_avatar.jpg',
+	})
+
+@app.route('/interests/<user_id>')
+def interests(user_id):
+	return jsonify([
+		'baseketball',
+		'poetry',
+		'music',
+		'heavy metal',
+		'computer science'
+	])
+
+@app.route('/friends/<user_id>')
+def friends(user_id):
+	return jsonify([
+		'Sam Lerner',
+		'Bobby Johnson',
+		'John Doe'
+	])
