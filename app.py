08a2176bdefbe71e42b3e6301aae9b204994ca28
Sam Lerner <lerner98@gmail.com>
2019-08-29 18:00:23 -0400

    friends returns twitter handles

--- app.py
+++ app.py
@@ -8,12 +8,28 @@ def index():
 
 @app.route('/user/<twitter_handle')
 def user(twitter_handle):
-	return jsonify({
-		'user_id': '1234',
-		'name': 'Sam Lerner',
-		'bio': 'OSU Computer Science. A in 5914',
-		'avatar': 'test_avatar.jpg',
-	})
+	if twitter_handle == 'samlerner':
+		return jsonify({
+			'user_id': '1234',
+			'name': 'Sam Lerner',
+			'bio': 'OSU Computer Science. A in 5914',
+			'avatar': 'test_avatar.jpg',
+		})
+	elif twitter_handle == 'bobbyj':
+		return jsonify({
+			'user_id': '2345',
+			'name': 'Bobby Johnson',
+			'bio': 'A generic name',
+			'avatar': 'test_avatar2.jpg',
+		})
+	elif twitter_handle == 'JD':
+		return jsonify({
+			'user_id': '3456',
+			'name': 'John Doe',
+			'bio': 'An even more generic name',
+			'avatar': 'test_avatar3.jpg',
+		})
+		
 
 @app.route('/interests/<user_id>')
 def interests(user_id):
@@ -28,7 +44,7 @@ def interests(user_id):
 @app.route('/friends/<user_id>')
 def friends(user_id):
 	return jsonify([
-		'Sam Lerner',
-		'Bobby Johnson',
-		'John Doe'
+		'samlerner',
+		'bobbyj',
+		'JD'
 	])
