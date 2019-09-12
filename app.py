3b194e849ed2755b27bd5f00cb919113396d091b
Sam Lerner <lerner98@gmail.com>
2019-09-12 17:46:49 -0400

    test push to master

--- app.py
+++ app.py
@@ -9,22 +9,26 @@ def index():
 @app.route('/user/<handle>')
 def user(handle):
 	if handle == '@mdo':
-		return jsonify({ 'first': 'Mark', 'last': 'Otto', 'handle': '@mdo' })
+		return jsonify({ 'first': 'Mark', 'last': 'Otto', 'handle': '@mdo' }), 200
 	if handle == '@fat':
-		return jsonify({ 'first': 'Jacob', 'last': 'Thornton', 'handle': '@fat' })
+		return jsonify({ 'first': 'Jacob', 'last': 'Thornton', 'handle': '@fat' }), 200
 	if handle == '@twitter':
-		return jsonify({ 'first': 'Larry', 'last': 'the Bird', 'handle': '@twitter' })
+		return jsonify({ 'first': 'Larry', 'last': 'the Bird', 'handle': '@twitter' }), 200
+
+	return 404
 
 @app.route('/interests/<handle>')
 def interests(handle):
 	if handle == "@slerner":
-		return jsonify(["Basketball", "Soccer", "Drawing", "Painting", "Archery", "Swimming"])
+		return jsonify(["Basketball", "Soccer", "Drawing", "Painting", "Archery", "Swimming"]), 200
 	elif handle == "@mdo":
-		return jsonify(["Basketball", "Soccer"])
+		return jsonify(["Basketball", "Soccer"]), 200
 	elif handle == "@fat":
-		return jsonify(["Drawing", "Soccer"])
+		return jsonify(["Drawing", "Soccer"]), 200
 	elif handle == "@twitter":
-		return jsonify(["Archery", "Painting", "Swimming"])
+		return jsonify(["Archery", "Painting", "Swimming"]), 200
+
+	return 404
 
 @app.route('/friends/<handle>')
 def friends(handle):
@@ -32,7 +36,7 @@ def friends(handle):
 		'@mdo',
 		'@fat',
 		'@twitter'
-	])
+	]), 200
 
 if __name__ == '__main__':
 	app.run(port=5000)
