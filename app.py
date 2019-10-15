5ba70bf96f60b7ba636a352020cf7120ea6629a9
Sam Lerner <lerner98@gmail.com>
2019-10-15 18:18:53 -0400

    added conversion from city to lat lng

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import os
 from os.path import join
 import sys
 sys.path.insert(0, join(os.getcwd(), 'tweet_api_src'))
+sys.path.insert(0, join(os.getcwd(), 'geo_service_src'))
 
+from geocode import get_coordinate
 from get_interests_dict_from_disco import get_interests_from_discovery
 from upload_all_user_tweets_to_discovery import upload_tweets_to_discovery
 from tweet_handler import get_tweets_by_location, get_tweets_by_user, get_users_by_tweets, upload_all_tweets_of_users
@@ -21,11 +23,11 @@ def index():
 @app.route('/user/<handle>', methods=['GET','POST'])
 def user(handle):
 	location_query = request.args.get('location')
-	location = '39.9611111,-82.9988889,50km'
-	''' Convert the location to lat,lng '''
+	location = ','.join(list(reversed([str(l) for l in get_coordinate(location_query)]))) + ',50km'
+
 	tweets = get_tweets_by_location(location)
 	users = list(get_users_by_tweets(tweets))
-	#users = users[:min(len(users), 3)]
+	users = users[:min(len(users), 3)]
 	upload_all_tweets_of_users(users, location_query)
 
 	tweet_list = get_tweets_by_user(handle)
@@ -34,11 +36,8 @@ def user(handle):
 
 @app.route('/friends_and_interests/<handle>/<location>', methods=['GET','POST'])
 def friends_and_interests(handle, location):
-	users_in_region = get_interests_from_discovery(location)
+	users_in_region = get_interests_from_discovery(location, handle)
 	most_alike_users = get_most_alike_to_user(handle, users_in_region)
-
-	print(most_alike_users)
-	print(handle)
 
 	interests = []
 
